603a8a3c8665510a1f21a0542f7acfa7179daacd
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-12-06 14:33:04 -0400

    added link to other main opage components

--- src/components/jobListing.jsx
+++ src/components/jobListing.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import {FaMapMarker} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
 
 const jobListing = ({job}) => {
 
@@ -45,12 +47,12 @@ const jobListing = ({job}) => {
           <FaMapMarker className="inline text-lg mb-1 mr-1"/>
           {job.location}
         </div>
-        <a
-          href= {`/job/${job.id}`}
+        <Link
+          to = {`/job/${job.id}`}
           className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
          Read More
-        </a>
+        </Link>
       </div>
     </div>
   </div>
