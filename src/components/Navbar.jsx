979aabbbb12bf2be646e97c4904e9bf737c92f96
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-12-06 14:30:21 -0400

    added layouts, job page and link instea dof a tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/images/logo.png'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -9,7 +10,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
             >
             {/* <!-- Logo --> */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center mr-4" to="/">
                 <img
                 className="h-10 w-auto"
                 src={ logo}
@@ -18,26 +19,26 @@ const Navbar = () => {
                 <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
                 >
-            </a>
-            <div className="md:ml-auto">
-                <div className="flex space-x-2">
-                <a
-                    href="/index.html"
-                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                    >Home</a
-                >
-                <a
-                    href="/jobs.html"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                    >Jobs</a
-                >
-                <a
-                    href="/add-job.html"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                    >Add Job</a
-                >
+            </Link>
+                <div className="md:ml-auto">
+                    <div className="flex space-x-2">
+                    <Link
+                        to="/"
+                        className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                        >Home
+                    </Link>
+                    <Link
+                        to="/jobs"
+                        className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                        >Jobs
+                    </Link>
+                    <Link
+                        to="/add-job"
+                        className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                        >Add Job
+                    </Link>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
         </div>
