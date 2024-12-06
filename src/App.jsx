d03aacf0c2cc0ca8598b98a53b8e0b9ec583b129
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-12-06 14:37:42 -0400

    added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import MainLayout from './layouts/MainLayout'
 
 import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element = {<MainLayout/>}>
       <Route index element = {<HomePage/>} />
       <Route path='/jobs' element = {<JobsPage/>} />
+      <Route path='*' element = {<NotFoundPage/>} />
     </Route>
     
   )
