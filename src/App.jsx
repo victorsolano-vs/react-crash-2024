2209ed091de9cf77bda05789cf33a6874449c0c5
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-12-06 20:36:27 -0400

    added add and delete job

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,52 @@ import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, {jobLoader} from './pages/JobPage'
+import AddJobPage from './pages/AddJobPage'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element = {<MainLayout/>}>
-      <Route index element = {<HomePage/>} />
-      <Route path='/jobs' element = {<JobsPage/>} />
-      <Route path='*' element = {<NotFoundPage/>} />
-      <Route path='/jobs/:id' element = {<JobPage/>} loader = {jobLoader}/>
-      {/* <Route path='/jobs/:id' element = {<JobPage/>} /> */}
-    </Route>
-    
-  )
 
-)
 
 const App = () => {
+
+// add job
+  const addJob =  async(newJob) => {
+    const res = await fetch('/api/jobs',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        
+      },
+      body: JSON.stringify(newJob)
+    })
+
+    return
+  }
+
+  // delete job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`,{
+      method: 'DELETE'
+    })
+
+    return
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element = {<MainLayout/>}>
+        <Route index element = {<HomePage/>} />
+        <Route path='/jobs' element = {<JobsPage/>} />
+        <Route path='/add-job' element = {<AddJobPage addJobSubmit={addJob}/>} />
+        <Route path='*' element = {<NotFoundPage/>} />
+        <Route path='/jobs/:id' element = {<JobPage deleteJob= {deleteJob}/>} loader = {jobLoader}/>
+        {/* <Route path='/jobs/:id' element = {<JobPage/>} /> */}
+      </Route>
+      
+    )
+  
+  )
+
+
+
   return (
     <RouterProvider router = {router}/>
   )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,12 +1,21 @@
 // import { useState, useEffect } from "react"
 import { useParams, useLoaderData } from "react-router-dom"
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa"
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
-const JobPage = () => {
-
+const JobPage = ({deleteJob}) => {
+    const navigate = useNavigate()
     const { id } = useParams()
     const job = useLoaderData()
+
+    const onDeleteClick = (jobId) => {
+      const confirm = window.confirm('are you sure you want to delete?')
+
+      if(!confirm) return
+
+      deleteJob(jobId)
+      navigate('/jobs')
+    }
     // const [job, setJob] = useState(null)
     // const [loading, setLoading] = useState(true)
 
@@ -106,6 +115,7 @@ const JobPage = () => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link>
               <button
+                onClick={()=> onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
