1dc4c31671347b349bf479cde14ea8ad7d90ce31
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-12-06 20:42:01 -0400

    added toastify

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -2,6 +2,7 @@
 import { useParams, useLoaderData } from "react-router-dom"
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa"
 import {Link, useNavigate} from 'react-router-dom'
+import { toast } from "react-toastify"
 
 const JobPage = ({deleteJob}) => {
     const navigate = useNavigate()
@@ -14,6 +15,7 @@ const JobPage = ({deleteJob}) => {
       if(!confirm) return
 
       deleteJob(jobId)
+      toast.success('Job deleted')
       navigate('/jobs')
     }
     // const [job, setJob] = useState(null)
