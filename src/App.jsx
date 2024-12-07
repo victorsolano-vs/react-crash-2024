72a21d60041bfa5dfb22521f5bdc989fd29afb19
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-12-06 21:01:46 -0400

    finished proj

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, {jobLoader} from './pages/JobPage'
 import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 
 
 
@@ -39,6 +40,18 @@ const App = () => {
 
     return
   }
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+        
+      },
+      body: JSON.stringify(job)
+    })
+
+    return
+  }
   
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -48,6 +61,7 @@ const App = () => {
         <Route path='/add-job' element = {<AddJobPage addJobSubmit={addJob}/>} />
         <Route path='*' element = {<NotFoundPage/>} />
         <Route path='/jobs/:id' element = {<JobPage deleteJob= {deleteJob}/>} loader = {jobLoader}/>
+        <Route path='/edit-job/:id' element = {<EditJobPage updateJobSubmit={updateJob}/>} loader = {jobLoader}/>
         {/* <Route path='/jobs/:id' element = {<JobPage/>} /> */}
       </Route>
       
